edab7ab9c26966eaabc0ec7c60576b3a86d21293
acapper <acapper56@gmail.com>
2019-04-12 14:58:20 +0100

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+arr=("odp" "pptx")
+log="presentation.log"
+old=""
+hide="false"
+
+function log () {
+	if [ ! -e $log ]; then
+		touch $log
+	fi
+	time=`date +"%Y-%m-%d %H:%M:%S"`
+	echo [$time]$1
+	echo [$time]$1 >> $log
+}
+
+function archive () {
+	log "[Archive] Begin archiving"
+	for f in *.odp
+	do
+		if [[ $f != ${1} ]]; then
+			date=`date +%Y-%m-%d`
+			arch="./archive/$date/"
+			mkdir -p ${arch}
+			mv "$f" ${arch}"$f"
+			log "[Archive] ${arch}${f}"
+		fi
+	done
+}
+
+log 
+log "[Start] Start"
+
+while getopts "h" flag; do
+	case "${flag}" in
+		h) hide="true"; log "[Flag] Hide flag set";;
+		*) exit 1 ;;
+	esac
+done
+
+inotifywait -m -e moved_to --format %f ~/Test | while read FILE
+do
+	if [[ " ${arr[*]} " == *${FILE##*.}* ]]; then
+		log "[Valid] Valid extension"
+		log "[New] New presentation: {$FILE}"
+		log "[Kill] Kill old presentation: {$old}"
+		old=$FILE
+		killall soffice.bin
+		log "[Open] Open new presentation: {${FILE}}"
+		if [ $hide == "true" ]; then
+			libreoffice --norestore "${FILE}" &
+		else
+			libreoffice --show --norestore "${FILE}" &
+		fi
+		archive "${FILE}"
+	else
+		log "[Invalid] Invalid extension: $FILE"
+	fi
+done
